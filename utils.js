a2eb299d09006c7396be3eacdfc161bb1acfb1ee
ysharipbaeva <sharipbaeva92@gmail.com>
2024-02-21 13:45:36 +0100

    refactoring

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const getRandomCharacter = () => characters.charAt(Math.floor(Math.random() * characters.length));
+
 const generateHash = stringLength => {
-  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const getItem = () => characters.charAt(Math.floor(Math.random() * characters.length));
-  const generatedString = Array(stringLength).fill(null).map(_ => getItem()).join('');
+  const generatedString = Array(stringLength).fill(null).map(_ => getRandomCharacter()).join('');
 
   if (!stringLength || typeof stringLength !== 'number') return 'string length must be type number';
   if (stringLength <= 0) return 'string length must be more then 0';
