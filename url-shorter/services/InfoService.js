54480aed01f68563729fc24fdc7cea7e674fb814
ysharipbaeva <sharipbaeva92@gmail.com>
2024-03-20 16:25:02 +0100

    homework7

---
+++ url-shorter/services/InfoService.js
@@ -0,0 +1,22 @@
+import InfoRepository from "../repository/infoRepository.js";
+import InfoModel from "../models/InfoModel.js";
+
+
+export default class InfoService{
+  constructor() {
+    this.infoRepository = new InfoRepository()
+  }
+
+  create(code, name, url, visits, created_time, user){
+    const info = new InfoModel(code, name, url, visits, created_time, user);
+    this.infoRepository.save(info);
+  }
+
+  getCode(code){
+    return this.infoRepository.get(code)
+  }
+
+  getCurrentUserUrl(user){
+    return this.infoRepository.getUserUrls(user)
+  }
+}
